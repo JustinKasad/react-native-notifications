07d62007a30bf85327db10b39006804053bbeeea
Justin Bolter <jbolter@JBOLTER-MAC.local>
2017-07-28 16:32:02 -0700

    Allow push when react is not initialized

--- android/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ android/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -60,7 +60,9 @@ public class PushNotification implements IPushNotification {
         // Comment this out to stop the notification from appearing in the status bar
         // This library relies on "title" and "body" properties in the payload which normal GCM payloads do not always have
         // We are handling this by firing a "local notification" in our app
-        //postNotification(null);
+        if (!mAppLifecycleFacade.isReactInitialized()) {
+            postNotification(null);
+        }
         notifyReceivedToJS();
     }
 
