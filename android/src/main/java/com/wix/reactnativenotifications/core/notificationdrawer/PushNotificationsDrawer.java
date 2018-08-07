3593e8f447acaf99cb654902c357372ea8b409fd
JustinK <justinkasad@gmail.com>
2018-08-07 11:55:26 -0700

    import statusbarnotification

--- android/src/main/java/com/wix/reactnativenotifications/core/notificationdrawer/PushNotificationsDrawer.java
+++ android/src/main/java/com/wix/reactnativenotifications/core/notificationdrawer/PushNotificationsDrawer.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 
+import android.service.notification.StatusBarNotification;
 import com.wix.reactnativenotifications.core.AppLaunchHelper;
 import com.wix.reactnativenotifications.core.InitialNotificationHolder;
 
